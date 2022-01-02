2474e8787d5c063554c2aa79b39d300c29a2d014
Hernán García <84438812+hernancur@users.noreply.github.com>
2022-01-02 05:05:05 -0300

    POST service for seed companies working, fixed Joi errors

--- api/src/validations/index.js
+++ api/src/validations/index.js
@@ -4,7 +4,7 @@ const seedCompanieValidation = Joi.object({
   name: Joi.string().required(),
   image: Joi.string().required(),
   url: Joi.string().required(),
-  lineage: Joi.array().optional(),
+  lineage: Joi.object().optional(),
   qr: Joi.string().optional(),
   description: Joi.string().optional(),
 });
