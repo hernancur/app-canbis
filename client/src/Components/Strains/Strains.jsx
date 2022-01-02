87a85c4a5914b6f3ad02885a0e0175a89eb2be55
Brian Cabrera <briancabrer4@gmail.com>
2022-01-02 04:48:18 -0300

    started to fetch data from backend

--- client/src/Components/Strains/Strains.jsx
+++ client/src/Components/Strains/Strains.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import Grid from '../Grid/Grid.jsx'
 import Navbar from "../Navbar/Navbar.jsx"
+import * as types from "../../Actions/types.js"
 
 export default function Strains() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch({
+            type: types.GET_STRAINS
+        })
+    })
+
     return (
         <div>
             <Navbar />
